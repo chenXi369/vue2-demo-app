9e0c176be064dff3f50e8c8169d22079715dc0c0
lichengcheng <2523826531@qq.com>
2023-11-01 19:57:31 +0800

    first commit

---
+++ component/CommonHeader/index.vue
@@ -0,0 +1,50 @@
+<template>
+	<view>
+		<header class="header">
+			<!-- 图片 -->
+			<!-- 相对路径（从'/'开始） -->
+			<image class="back" src="/static/logo.png" mode=""></image>
+			<view class="title">{{ title }}</view>
+		</header>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: 'commonHeader',
+		props: {
+			title: {
+				type: String,
+				default: ''
+			}
+		},
+		data() {
+			return {
+				
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.header {
+		width: 100%;
+		line-height: 88rpx;
+		text-align: center;
+		position: relative; // 相对
+		border-bottom: 1rpx solid #eaeaea;
+		
+		.back {
+			width: 30rpx;
+			height: 30rpx;
+			position: absolute;
+			top: 30rpx;
+			left: 24rpx;
+		}
+		.title {
+			font-size: 30rpx;
+			color: #1890ff;
+			font-weight: 600;
+		}
+	}
+</style>
---
+++ pages/login/login.vue
@@ -0,0 +1,75 @@
+<template>
+	<view class="login-page">
+		<!-- html5 (增加语义化和可读性) -->
+		<common-header :title="title"></common-header>
+		
+		<main class="main">
+			<!-- 登录的表单 --> 
+			<!-- <div></div> <span></span> -->
+			<view class="label-value">
+				<text>用户名</text>
+				<u-input v-model="loginForm.username"></u-input>
+			</view>
+			<view class="label-value">
+				<text>密码</text>
+				<u-input v-model="loginForm.password"></u-input>
+			</view>
+		</main>
+		
+		<footer class="footer">
+			<u-button type="primary" @click="submitLogin">提交</u-button>
+		</footer>
+	</view>
+</template>
+
+<script>
+	// import CommonHeader from '@/components/CommonHeader/index.vue'
+	
+	export default {
+		// components: {
+		// 	CommonHeader
+		// },
+		data() {
+			return {
+				title: '登录',
+				loginForm: {
+					username: null,
+					password: null
+				}
+			}
+		},
+		methods: {
+			submitLogin() {
+				// 调接口的操作
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.login-page {
+		width: 100%;
+		height: 100vh;
+		position: relative;
+		
+		// 登录的表单
+		.main {
+			width: calc(100% - 100rpx);  // 动态的
+			position: absolute;
+			left: 50rpx;
+			top: 300rpx;
+			background: #eaeaea;
+			border-radius: 20rpx;
+			height: 300rpx;
+			padding: 30rpx 24rpx;  // 盒子模型 => ie 正常
+			box-sizing: border-box;  // 正常
+		}
+		
+		.footer {
+			width: calc(100% - 100rpx);
+			position: fixed;
+			bottom: 100rpx;
+			left: 50rpx;
+		}
+	}
+</style>
